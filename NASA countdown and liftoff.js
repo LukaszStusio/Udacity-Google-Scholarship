14d79e73cfa5520e5115a156e4cc25889a320c56
LukaszStusio <lukasz.stusio@gmail.com>
2018-01-21 00:49:55 +0100

    countdown preparations

---
+++ NASA countdown and liftoff.js
@@ -0,0 +1,20 @@
+/*
+    Tasks durin countdown:
+          Orbiter transfers from ground to internal power (T-50 seconds)
+          Ground launch sequencer is go for auto sequence start (T-31 seconds)
+          Activate launch pad sound suppression system (T-16 seconds)
+          Activate main engine hydrogen burnoff system (T-10 seconds)
+          Main engine start (T-6 seconds)
+          Solid rocket booster ignition and liftoff! (T-0 seconds)
+
+        If there's a task being completed, it prints out the task
+        If there is no task being completed, it prints out the time as T-x seconds
+
+ * Programming Quiz: Countdown, Liftoff! (4-3)
+ *
+ * Using a while loop, print out the countdown output above.
+ */
+
+// your code goes here
+
+var time = 60;
