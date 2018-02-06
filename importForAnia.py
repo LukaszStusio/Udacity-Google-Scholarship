1219aa8a2c1fc463c123339be01de1bc4ef7bfb6
LukaszStusio <lukasz.stusio@gmail.com>
2018-02-06 19:49:54 +0100

    first array split()

--- importForAnia.py
+++
@@ -1,7 +0,0 @@
-import pygame as pg
-import urllib.request
-pg.init()
-url=urllib.request.urlopen("http://66.90.93.122/ost/the-nightmare-before-christmas-oogie-s-revenge/agbighfe/01%20This%20is%20Halloween.mp3").read()
-pygame.mixer.init()
-sample=pg.mixer.Sound(url)
-sample.play()
