7baf9bd8179f2d776299804bcbf0baa6aed0a98f
LukaszStusio <lukasz.stusio@gmail.com>
2018-02-08 18:55:26 +0100

    solving a trouble from work - trimming unwanted space gap in strings, with a big help from krzysztof83

---
+++ trimTheAdWords.js
@@ -0,0 +1,9 @@
+
+
+function trimKeywords(){
+  var keywords = ["  shoes black "," jhoh lhoh","ijl kjlkj  "];
+    for(var i = 0; i < keywords.length; i++){
+        keywords[i] = keywords[i].trim;
+        console.log(keywords[i]);
+    }
+}
